0f6668ddb8f2cfbd0e7ebb856cac68cdb05cef4e
Rudisons <brokolis178@gmail.com>
2022-02-17 14:56:29 +0200

    lol

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function nolasa (m1,m2,m3){
     const malas=document.getElementsByClassName("mala");
-    const m1=parseFloat(mala1.value);
+    m1=parseFloat(mala1.value);
+    m2=parseFloat(mala2.value);
+    m3=parseFloat(mala3.value);
     
     if(m1>0&&m2>0&&m3>0){
         console.log({m1,m2,m3});
