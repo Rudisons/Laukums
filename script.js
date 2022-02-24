ae3969126977615bf0f4f221f21ae51a55ece918
Rudisons <brokolis178@gmail.com>
2022-02-24 14:54:05 +0200

    lielaa zive

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ function nolasa (m1,m2,m3){
     }
     
 }
+function perimetrs(m1,m2,m3){
+    const m=nolasa();
+    m1=m.m1;m2=m.m2;m3=m.m3;
+    console.log(m1,m2,m3);
+    const p=m1+m2+m3;
+    console.log(p);
+    return(p);
+}
+function pusperimetrs(m1,m2,m3){
+    const m=nolasa();
+    m1=m.m1;m2=m.m2;m3=m.m3;
+    console.log(m1,m2,m3);
+    const pusperimetrs=(m1+m2+m3)/2;
+    console.log(pusperimetrs);
+    return(pusperimetrs);
+}
+
+function laukums(m1,m2,m3){
+    const pusperimetrs=perimetrs(m1,m2,m3)/2;
+   let laukums=Math.sqrt(pusperimetrs*(pusperimetrs-m1)*(pusperimetrs-m2)*(pusperimetrs-m3));
+    console.log(laukums);
+    return(laukums);
+}
+function trijstūris(m1,m2,m3){
+if(m1,m2+m3&&m2<m1+m3&&m3<m1+m2){
+    return true;
+}else{
+return false;
+}
+}
